77354ac1d18566b34c741b301d45835720e73be3
Prajnarao0318 <134444402+Prajnarao0318@users.noreply.github.com>
2023-09-13 20:37:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,59 +1,67 @@
 const express=require("express")
-const app=express()
 const mongoose=require("mongoose")
-app.set('view engine','ejs');
-const https=require("https")
 const body=require("body-parser")
+const app=express()
+app.set('view engine', 'ejs');
 app.use(body.urlencoded({extended:true}))
 app.use(express.static("public"))
 
-mongoose.connect("mongodb+srv://prajnarao002:<prajnarao03>@cluster.f2lxtll.mongodb.net/tododb",{useNewUrlParser:true})
-// var lists=[]
+
+
+mongoose.connect("mongodb+srv://sudeep:test123@cluster0.mpx93rw.mongodb.net/tododb",{useNewUrlParser:true})
 
 //schema
 const todoschema=new mongoose.Schema({task:String})
-
 //model
 const todomodel=mongoose.model("tasks",todoschema)
 
 // const t1=new todomodel({task:"gaming"})
+// const t2=new todomodel({task:"studying"})
+// const t3=new todomodel({task:"playing"})
+
+// t2.save()
+// t3.save()
 
 // t1.save()
+
+
 
 
 app.get("/",function(req,res){
 
     todomodel.find().then((result) => {
-        res.render("index",{tasks:result})
-    }).catch((err)=>{
+        res.render('index',{tasks:result})
+    }).catch((err) => {
         console.log(err)
     });
+
+
+    
 })
 
 app.post("/",function(req,res){
-    var todotask=req.body.task
-    // console.log(task)
-    // lists.push(task)
+var todotask=req.body.task
+// console.log(task)
+// lists.push(task)
+const task=new todomodel({task:todotask})
 
-    const task=new todomodel({task:todotask})
+task.save()
 
-    task.save()
+res.redirect("/")
 
-    res.redirect("/")
 })
+
 
 app.post("/delete",function(req,res){
     var item=req.body.checkbox
+   todomodel.deleteOne({_id:item}).then((result) => {
+    res.redirect("/")
+   }).catch((err) => {
+    console.log(err)
+   });
 
-    todomodel.deleteOne({_id:item}).then((result)=>{
-        res.redirect("/")
-    }).catch((err)=>{
-        console.log(err)
-    });
-
-    // console.log(item)
 })
 
-app.listen(  3001,function(){
-    console.log("server is running in 3001")
+app.listen(process.env.PORT ||3000,function(){
+    console.log("Server is up and running")
 })
