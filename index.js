6bcf45ae30e1d7064f1972826de76aa033de17d5
Prajnarao0318 <prajnarao002@gmail.com>
2023-09-13 08:09:31 +0530

    mini-project6 commit

---
+++ index.js
@@ -0,0 +1,59 @@
+const express=require("express")
+const app=express()
+const mongoose=require("mongoose")
+app.set('view engine','ejs');
+const https=require("https")
+const body=require("body-parser")
+app.use(body.urlencoded({extended:true}))
+app.use(express.static("public"))
+
+mongoose.connect("mongodb://127.0.0.1:27017/tododb",{useNewUrlParser:true})
+// var lists=[]
+
+//schema
+const todoschema=new mongoose.Schema({task:String})
+
+//model
+const todomodel=mongoose.model("tasks",todoschema)
+
+// const t1=new todomodel({task:"gaming"})
+
+// t1.save()
+
+
+app.get("/",function(req,res){
+
+    todomodel.find().then((result) => {
+        res.render("index",{tasks:result})
+    }).catch((err)=>{
+        console.log(err)
+    });
+})
+
+app.post("/",function(req,res){
+    var todotask=req.body.task
+    // console.log(task)
+    // lists.push(task)
+
+    const task=new todomodel({task:todotask})
+
+    task.save()
+
+    res.redirect("/")
+})
+
+app.post("/delete",function(req,res){
+    var item=req.body.checkbox
+
+    todomodel.deleteOne({_id:item}).then((result)=>{
+        res.redirect("/")
+    }).catch((err)=>{
+        console.log(err)
+    });
+
+    // console.log(item)
+})
+
+app.listen( process.env.PORT ||3001,function(){
+    console.log("server is running in 3001")
+})
